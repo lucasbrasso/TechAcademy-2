fc9cae7f449bf11a1eca498ff8d5190917b3a651
lucasbrasso <161781331+lucasbrasso@users.noreply.github.com>
2024-06-22 01:47:24 -0300

    Pagina de contato com Bootstrap

---
+++ GameHeaven Site/pages/contato.php
@@ -0,0 +1,13 @@
+<div class="contato">
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Seu Email</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="nome@exemplo.com">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Sua mensagem</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="8"></textarea>
+  <a>Enviar</a>
+  <a>Voltar</a>
+</div>
+    
+</div>
